1f2fe16aab30ac7fc6b44dbd535370e12a903c88
Nils Gaukroger <nilsgaukroger@gmail.com>
2021-09-18 12:45:58 +0200

    Started producing comprehensive aero_design.m
    Removed all the superfluous code from aero_design_exercise and
    started formatting residuals() to work for various spanwise positions.
    Will code it such that we can vary tsr etc. from the exercises and
    then for Report #1 we can change the design polynomials.

--- aero_design_exercise/aero_design.m
+++ aero_design_exercise/aero_design.m
@@ -10,16 +10,82 @@ rotor.R     = 35;  % length of blade [m]
 rotor.tsr   = 9.0; % TSR [-]
 rotor.B     = 3;   % number of blades [-]
 rotor.a     = 1/3; % axial induction [-]
-rotor.r     = 8;   % blade span [m]
+
+
+spacing     = 0.5; % increment for spanwise discretisation [m]
+rotor.r_lst = 5:spacing:rotor.R-spacing;   % blade span [m]
+
+
+
+
 rotor.t     = 1.5; % absolute thickness...
              % at blade span [m]
 rotor.cl    = 0.6; % design lift coefficient [-]
 rotor.clcd  = 120; % design cl/cd [-]
 rotor.alpha = 4;   % design AoA [deg]
 
-
-
 %% Functions
+
+function [out,varargout] = residuals(x,rotor,varargin)
+% Calculate the residuals for chord and tangential induction factor
+
+% unpack inputs
+R = rotor.R; tsr = rotor.tsr;
+B = rotor.B; a = rotor.a;
+
+% spanwise position
+if nargin == 2
+    r = rotor.r;
+elseif nargin > 2
+    r = rotor{1};
+end
+
+% unpack x
+c  = x(1);
+ap = x(2);
+
+% calculate that, cl and cl/cd from design polynomials
+t    = thickness(r);
+that = t / c;
+cl   = cl_des(that*100);
+clcd = clcd_des(that*100);
+
+% calculate intermediate variables
+phi   = atan(((1-a)/(1+ap)) * (R/(r*tsr)));
+cd    = cl/clcd;
+cy    = cl * cos(phi) + cd * sin(phi);
+cx    = cl * sin(phi) - cd * cos(phi);
+f     = (B/2) * ((R-r)/(r * sin(phi)));
+F     = (2/pi) * acos(exp(-f));
+sigma = (c*B) / (2*pi*r);
+
+% calculate residuals
+res_c  = 4*pi*r * sin(phi)^2 * F * ((2*a) / ...
+    (cy * B * (1-a))) - c;
+res_ap = 1 / ((4 * F * sin(phi) * cos(phi) / ...
+    (sigma * cx)) - 1) - ap;
+
+% pack output
+out = [res_c, res_ap];
+
+% optional outputs
+if nargout == 2
+    rotor.t = t;
+    rotor.c = c;
+    rotor.phi = phi;
+    rotor.alpha = alpha_des(that);
+    rotor.beta = phi - alpha_des(that);
+    rotor.cl = cl;
+    rotor.cd = cd;
+    rotor.ap = ap;
+    rotor.cp = ((1-a)^2 + (tsr*(r/R)) * (1+ap)^2) * tsr * (r/R) * sigma * cx;
+    rotor.ct = ((1-a)^2 + (tsr*(r/R)) * (1+ap)^2) * sigma * cy;
+    % rotor.a     = 1 / (((4*F*np.sin(phi)**2) / (sigma * cy)) + 1)
+    varargout{1} = rotor;
+end
+end
+
+%% Design polynomials for the 35-m blade
 
 function t = thickness(r)
 % Absolute thickness [m] as a function of radius [m] for 35m blade
@@ -61,46 +127,4 @@ for i = 1:length(that)
         alpha(i) = -0.0078*that(i) + 0.7813;
     end
 end
-end
-
-function out = residuals(x,inputs,varargin)
-% Calculate the residuals for chord and tangential induction factor
-
-% unpack inputs
-R = inputs.R; tsr = inputs.tsr;
-B = inputs.B; a = inputs.a;
-
-% spanwise position
-if nargin == 2
-    r = inputs.r;
-elseif nargin > 2
-    r = varargin{1};
-end
-
-% unpack x
-c  = x(1);
-ap = x(2);
-
-% calculate that, cl and cl/cd from design polynomials
-that = thickness(r) / c;
-cl   = cl_des(that*100);
-clcd = clcd_des(that*100);
-
-% calculate intermediate variables
-phi   = atan(((1-a)/(1+ap)) * (R/(r*tsr)));
-cd    = cl/clcd;
-cy    = cl * cos(phi) + cd * sin(phi);
-cx    = cl * sin(phi) - cd * cos(phi);
-f     = (B/2) * ((R-r)/(r * sin(phi)));
-F     = (2/pi) * acos(exp(-f));
-sigma = (c*B) / (2*pi*r);
-
-% calculate residuals
-res_c  = 4*pi*r * sin(phi)^2 * F * ((2*a) / ...
-    (cy * B * (1-a))) - c;
-res_ap = 1 / ((4 * F * sin(phi) * cos(phi) / ...
-    (sigma * cx)) - 1) - ap;
-
-% pack output
-out = [res_c, res_ap];
 end

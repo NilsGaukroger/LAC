4012c00b04bcc82aea7247b1ebaa0006547813ba
Nils Gaukroger <nilsgaukroger@gmail.com>
2021-09-30 09:11:20 +0200

    made separate function files

---
+++ report_1/polars.m
@@ -0,0 +1,15 @@
+function aerofoil = polars()
+% extract aerofoil input data
+
+S = dir('polars\*.txt');
+S = S(1:6,1); % remove 'hints.txt'
+aerofoil = cell(2,length(S));
+aerofoil(1,:) = {S.name};
+vars = {'alpha','cl','cd','fsst','clinv','clfs','cm'};
+
+for i = 1:size(aerofoil,2)
+    aerofoil{2,i} = readtable("polars\" + aerofoil{1,i});
+    aerofoil{2,i}.Properties.VariableNames = vars;
+    aerofoil{1,i} = erase(aerofoil{1,i},"_ds.txt");
+end
+end

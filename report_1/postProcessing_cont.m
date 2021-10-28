42964e09903c590b7167c07e232455f414bc1c01
Nils Gaukroger <nilsgaukroger@gmail.com>
2021-10-28 16:37:56 +0200

    started post-processing script for extracting gains from *_tuning.txt

---
+++ report_1/postProcessing_cont.m
@@ -0,0 +1,10 @@
+% 46320 LAC Course
+% Assignment 3: Post-processing
+% Authors: Nikolaos Stamatopoulos, Nils Joseph Gaukroger, Stefanos Masalas,
+% Eduardo Fredrich de Miranda
+% 28th October 2021
+
+close all; clear variables; clc
+
+%% Load data from postProcessing_stab
+load('postProcessing_stab.mat');

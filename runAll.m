8e476aa746652328957824ef32d419cdc2ea221b
Nils Gaukroger <nilsgaukroger@gmail.com>
2021-11-11 12:13:48 +0100

    updated runAll.m to be run from home directory

--- runAll.m
+++ runAll.m
@@ -8,14 +8,16 @@ close all; clear variables; clc
 
 %% Run all scripts
 % Assignment 1: Aerodynamic rotor design for IEC class IIIb
-aero_design
+run('A1/scripts/aero_design')
 % Assignment 1: Writing inputs for HAWC
-HAWC_inputs
+run('A1/scripts/HAWC_inputs')
+% Assignment 1: Aerodynamic post-processing
+run('A1/scripts/postProcessing_aero')
 % Assignment 2: Scaling of structural parameters
-Structural_scaling
+run('A2/scripts/Structural_scaling')
 % Assignment 2: Structural post-processing
-postProcessing_struct
+run('A2/scripts/postProcessing_struct')
 % Assignment 2: Stability post-processing
-postProcessing_stab
+run('A2/scripts/postProcessing_stab')
 % Assignment 3: Controller design post-processing
-postProcessing_cont
+run('A2/scripts/postProcessing_cont')
